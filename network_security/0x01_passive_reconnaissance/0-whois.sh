a9b24eeeffbfd18945b5850fd9ed9b5dd77d95ee
agayevaelvina <elvinaagyeva@gmail.com>
2025-08-14 08:11:37 +0000

    x

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '
-/^Registrant Organization:/ {
-  print "Registrant Organization," $2
-}
-/^Tech Email:/ {
-  print "Tech Email," $2
-}' > "$1.csv"
+whois holbertonschool.com
