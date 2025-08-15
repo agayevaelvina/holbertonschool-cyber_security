401feb2e9c924b1a52273de6b97c61c76b060b1d
agayevaelvina <elvinaagyeva@gmail.com>
2025-08-15 08:32:51 +0000

    x

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk 'BEGIN{FS=":";OFS=","} /^Registrant/||/^Admin/||/^Tech/ {section=$1; sub(/[ \t].*/, "", section); key=$1; sub(/^[^:]+:[ \t]*/, "", $0); gsub(/ /,"$", $0); if(!h[section]++){print section key} print section $0}' > "$1.csv"
+whois "$1" | awk 'BEGIN{FS=":";OFS=","} /^[RA]dmin|^Registrant|^Tech/ {s=$1; sub(/[ \t].*/, "", s); k=$1; sub(/^[^:]+:[ \t]*/, "", $0); gsub(/ +/, " ", $0); gsub(/^ +| +$/, "", $0); sub(/^ +/, "", k); sub(/^ +| +$/, "", k); if(!h[s k]++){print s" "k,$0}' > "$1".csv
