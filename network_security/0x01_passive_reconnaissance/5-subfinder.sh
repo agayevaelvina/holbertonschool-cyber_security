8601b3894983b8372942b550bee512f9d20fb09e
agayevaelvina <elvinaagyeva@gmail.com>
2025-08-15 05:44:58 +0000

    x

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -silent -d $1 | while read d; do ip=$(dig +short "$d" | grep -m1 '^[0-9.]\+$'); echo "$d,${ip:-NotResolved}"; done > "$1.txt"
+subfinder -silent -d $1 -o $1.txt -nW -oI
