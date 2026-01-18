4423ee9579a40486962ec76170aa50f0ae524b29
elna <elvinaagyeva@gmail.com>
2026-01-18 13:11:36 +0400

    z

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,9 +1,20 @@
+#!/usr/bin/env ruby
 require 'json'
 
-def read_json_file(file_path)
-  file_content = File.read(file_path)
-  JSON.parse(file_content)
-rescue Errno::ENOENT
-  {}
+def count_user_ids(path)
+
+  file = File.read(path)
+
+  data = JSON.parse(file)
+
+  counts = Hash.new(0)
+
+  data.each do |item|
+    user_id = item['userId']
+    counts[user_id] += 1
+  end
+  counts.sort.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
 end
 
