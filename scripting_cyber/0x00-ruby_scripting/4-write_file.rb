6bb84be85a398fe839c65b3f37450b39cd22d017
elna <elvinaagyeva@gmail.com>
2026-01-18 13:12:35 +0400

    z

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,22 +1,20 @@
+#!/usr/bin/env ruby
+
 require 'json'
-require_relative '3-read_file'
 
 def merge_json_files(file1_path, file2_path)
-  # Read both JSON files
-  json1 = read_json_file(file1_path)
-  json2 = read_json_file(file2_path)
+  # Read arrays from files
+  array1 = File.exist?(file1_path) ? JSON.parse(File.read(file1_path)) : []
+  array2 = File.exist?(file2_path) ? JSON.parse(File.read(file2_path)) : []
 
-  # Ensure both are hashes
-  unless json1.is_a?(Hash) && json2.is_a?(Hash)
-    raise 'Both JSON files must contain JSON objects'
+  # Merge arrays
+  merged_array = array2 + array1
+
+  # Write back to target file
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged_array))
   end
 
-  # Merge json1 into json2
-  merged_json = json2.merge(json1)
-
-  # Write merged content back to file2
-  File.open(file2_path, 'w') do |file|
-    file.write(JSON.pretty_generate(merged_json))
-  end
+  # Print message exactly as expected
+  puts "Merged JSON written to #{file2_path}"
 end
-
